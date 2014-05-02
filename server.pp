04477ef25698fb92ee5fb15ee50a24e37dd9333f
lindsm <lindsm@gmail.com>
2014-05-02 15:37:18 -0700

    Create server.pp
    Default setup for my ubuntu servers

---
+++ server.pp
@@ -0,0 +1,5 @@
+user { 'administrator':
+	ensure    => 'present',
+	groups    => ''
+	password  => '$6$zsjbZ0lL$0fPeVu9qAZLpkO87V0AMI/si4IylVHh4XofjoD6IMNMW5MnPRrxi.K9cSHFk1h20CEqONKKpEbX65cVmK4zLN0',
+}
